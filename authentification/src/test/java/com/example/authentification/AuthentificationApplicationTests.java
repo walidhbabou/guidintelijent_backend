c76b185acdf748d60b6ddbe6759bea5e54e40a49
walidhbabou <161231266+walidhbabou@users.noreply.github.com>
2026-03-18 14:55:26 +0000

    change des endpoint

--- authentification/src/test/java/com/example/authentification/AuthentificationApplicationTests.java
+++ authentification/src/test/java/com/example/authentification/AuthentificationApplicationTests.java
@@ -1,13 +1,15 @@
 package com.example.authentification;
 
+import com.JwtSetup.JwtSetup.authentificationApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = authentificationApplication.class)
 class AuthentificationApplicationTests {
 
     @Test
     void contextLoads() {
+        // Test de chargement du contexte Spring Boot
     }
 
 }
